ab1c7c2fc84d26b6e18e0083ce9f961395fd0b23
PandeKadekGalihNataLesmana <101319709+PandeKadekGalihNataLesmana@users.noreply.github.com>
2022-03-10 17:45:19 +0800

    Add files via upload

---
+++ Tugas1/q1_sample.sql
@@ -0,0 +1,12 @@
+ SELECT CategoryName
+   ...> FROM Category
+   ...> ORDER BY CategoryName ASC;
+Beverages
+Condiments
+Confections
+Dairy Products
+Grains/Cereals
+Meat/Poultry
+Produce
+Seafood
+sqlite>
